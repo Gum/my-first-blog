5eacfc2f632391901035403b6528ef6f72f2ab19
Nata <nata@monoku.com>
2018-06-09 12:17:58 -0500

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,32 @@
+
+
+# print("qué número de volumen?")
+# volume = int(input())
+# if volume < 20:
+# 	print("como callado")
+# elif 20 <= volume < 40:
+#     print("It's nice for background music")
+# elif 40 <= volume < 60:
+#     print("Perfect, I can hear all the details")
+# elif 60 <= volume < 80:
+#     print("Nice for parties")
+# elif 80 <= volume < 100:
+#     print("A bit loud!")
+# else:
+#     print("My ears are hurting! :(")
+
+
+# def hi(name):
+#     print("Hi " + name + "!")
+
+# # hi("Nata")
+
+# girls = ["Angela", "Pamela", "Sandra", "Rita"]
+
+# for name in girls:
+# 	hi(name)
+# 	print('y')
+
+
+for x in range(1,6):
+	print(x)
